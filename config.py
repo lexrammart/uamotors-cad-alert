ffb1c294b9fb5511fd0a0b89ecaa8e94b38b208c
lexrammart <81694980+lexrammart@users.noreply.github.com>
2026-08-03 23:30:46 -0600

    Update notification base name and regex patterns for assembly files

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ WEBHOOK_URL = "https://discord.com/api/webhooks/1533988281195171962/nv3cJEvWrUhc
 TARGET_FOLDER = "UAMOTORS"
 
 # Base name used for notifications
-BASE_NAME = "GENERAL ASSEMBLY"
+BASE_NAME = "OP-01assembly"
 
 # Regular expressions to identify assembly versions and temporary lock files
-ASSEMBLY_PATTERN = r"^GENERAL ASSEMBLY v\d+\.SLDASM$"
-LOCK_PATTERN = r"^~\$GENERAL ASSEMBLY v\d+\.SLDASM$"
+ASSEMBLY_PATTERN = r"^OP-01assembly\d+\.SLDASM$"
+LOCK_PATTERN = r"^~\$OP-01assembly\d+\.SLDASM$"
 
 # Local installation environment configuration
 INSTALL_FOLDER_NAME = "UAMotorsCAD"
